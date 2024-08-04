e2c7b5927f9790146d31dbf84a83c0cce07db578
Albérico Eduardo <albericoeduardo202@gmail.com>
2024-08-04 07:41:08 -0300

    feat: add model package and model files

---
+++ test-oj/src/main/java/org/acme/model/Project.java
@@ -0,0 +1,33 @@
+package org.acme.model;
+
+public class Project {
+    private Long id;
+    private String image;
+    private String title;
+    private String description;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
