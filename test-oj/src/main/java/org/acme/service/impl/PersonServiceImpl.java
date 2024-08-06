18471f9ba617ea8174ef4540ad79d42d176ebe82
alberico <asoares@gdmseeds.com>
2024-08-06 15:30:55 -0300

    refactor: delete code who will be present in resource logic

--- test-oj/src/main/java/org/acme/service/impl/PersonServiceImpl.java
+++ test-oj/src/main/java/org/acme/service/impl/PersonServiceImpl.java
@@ -7,38 +7,23 @@ import java.util.Optional;
 import org.acme.entity.Person;
 import org.acme.service.PersonService;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/team")
 public class PersonServiceImpl implements PersonService{
 
     public List<Person> team = new ArrayList<>();
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
     @Override
     public Response getPerson() {
         return Response.ok(team).build();
     }
 
-    @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.TEXT_PLAIN)
     @Override
     public Response createPerson(Person newTeam) {
         team.add(newTeam);
         return Response.status(Response.Status.CREATED).entity(newTeam).build();
     }
 
-    @DELETE
-    @Consumes(MediaType.TEXT_PLAIN)
     @Override
     public Response deletePerson(Long id) {
         Optional<Person> teamToDelete = team.stream().filter(team -> team.getId().equals(id)).findFirst();
@@ -52,5 +37,4 @@ public class PersonServiceImpl implements PersonService{
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
-    
 }
--- test-oj/src/main/java/org/acme/service/impl/ProjectsServiceImpl.java
+++ test-oj/src/main/java/org/acme/service/impl/ProjectsServiceImpl.java
@@ -7,39 +7,23 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Optional;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/projects")
 public class ProjectsServiceImpl implements ProjectsService{
 
     public List<Projects> projects= new ArrayList<>();
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
     @Override
     public Response getProject() {
         return Response.ok(projects).build();
     }
 
-    @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.TEXT_PLAIN)
     @Override
     public Response createProject(Projects newProject) {
         projects.add(newProject);
         return Response.status(Response.Status.CREATED).entity(newProject).build();
     }
 
-    @DELETE
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Path("{id}")
     @Override
     public Response deleteProject(Long id) {
         Optional<Projects> projectToDelete = projects.stream().filter(projects -> projects.getId().equals(id)).findFirst();
