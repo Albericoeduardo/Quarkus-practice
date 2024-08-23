ebaf81cdef5c53fd3cccc3dd60d6a630bc0034ea
alberico <asoares@gdmseeds.com>
2024-08-23 13:50:31 -0300

    refactor: delete dead code

--- test-oj/src/main/java/org/acme/service/impl/NewsServiceImpl.java
+++ test-oj/src/main/java/org/acme/service/impl/NewsServiceImpl.java
@@ -1,10 +1,7 @@
 package org.acme.service.impl;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
